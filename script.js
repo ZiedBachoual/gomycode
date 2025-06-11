c215752245561807bc721e09e444382e6e4baa8a
Zied BACHOUAL <ziedbachoual@outlook.fr>
2025-06-11 01:16:52 +0100

    Update : add javascript and refactor css

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeMessage() {
+  const title = document.getElementById('welcome-title');
+  const message = document.getElementById('welcome-message');
+
+  title.textContent = "Hello les étudiants !";
+  message.textContent = "Prêts à coder et apprendre ensemble ?";
+}
